a8ed8e3c9eaa6f4fb5656600ed243ac93a023fa2
TheWisePigeon <zozozozeph@gmail.com>
2023-04-18 22:45:59 +0000

    docs: add small description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
     import "../app.css"
 </script>
 
-<h1 class=" text-red-300">SOmething</h1>
+<header class=" flex justify-between py-5 border-b">
+    <a href="/">
+        <h1 class=" text-2xl ">IRA</h1>
+    </a>
+    <div class=" flex gap-10 underline">
+        <a href="https://github.com/TheWisePigeon/ira">GitHub</a>
+        <a href="/docs">Docs</a>
+    </div>
+</header>
+<slot/>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script></script>
+
+<div class=" text-xl">
+	<h1>
+		IRA is a tool that helps you replicate your dev environment accross many devices, easily by just
+		creating a config file
+	</h1>
+	<h1>
+		I made it after all my developer friends randomly decided to switch to neovim and since I was
+		the Neovim "veteran", I had to help then download, install and configure it on their devices, in
+		total we did it like 6 times that day, 2 times on windows, 2 times on wsl and 2 times on ubuntu.
+		I wrote the tool when I got home that night
+	</h1>
+	<h1 class=" font-bold">
+		IRA means "anger" in latin, and I was pretty pissed off while I was writting it, so that's where
+		the name came from :)
+	</h1>
+    <h1 class="text-3xl text-red-500 mt-5 underline">INSTALLATION</h1>
+</div>
